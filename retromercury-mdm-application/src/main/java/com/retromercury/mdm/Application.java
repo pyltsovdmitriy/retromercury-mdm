cf1bd9b3872b537b064d0b0bab4c001b2aa3a401
dpyltsov <h-e-l-ls-i-n-g@mail.ru>
2024-10-10 02:35:02 +0400

    add start message

--- retromercury-mdm-application/src/main/java/com/retromercury/mdm/Application.java
+++ retromercury-mdm-application/src/main/java/com/retromercury/mdm/Application.java
@@ -1,13 +1,21 @@
 package com.retromercury.mdm;
 
-
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class);
-        System.out.println("Hello world!");
+        log.info("""
+                \"""
+                ======================================================================================================================
+                ======================================================================================================================
+                ====================================================Server Started====================================================
+                ======================================================================================================================
+                ======================================================================================================================
+                """);
     }
 }
