e946e6a7d1ea07f6eaa3744bdae3b420dee8df7f
dpyltsov <h-e-l-ls-i-n-g@mail.ru>
2024-10-09 23:59:32 +0400

    first commit

---
+++ src/main/java/com/retromercury/mdm/RetromercuryMdmApplication.java
@@ -0,0 +1,13 @@
+package com.retromercury.mdm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetromercuryMdmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetromercuryMdmApplication.class, args);
+	}
+
+}
