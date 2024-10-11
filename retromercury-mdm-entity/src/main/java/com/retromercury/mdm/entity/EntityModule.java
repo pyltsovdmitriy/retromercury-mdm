de92351e1daf57ae9ac5e925f97b23fd0cd08596
dpyltsov <h-e-l-ls-i-n-g@mail.ru>
2024-10-12 02:37:27 +0400

    dockerfile created

---
+++ retromercury-mdm-entity/src/main/java/com/retromercury/mdm/entity/EntityModule.java
@@ -0,0 +1,13 @@
+package com.retromercury.mdm.entity;
+
+
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class EntityModule {
+
+    public void show() {
+        System.out.println("I am Entity");
+    }
+}
